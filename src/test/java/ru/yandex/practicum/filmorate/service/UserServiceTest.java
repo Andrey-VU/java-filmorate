dfe6326bcec8826238641022a1603d1092b92ac8
Andrey-VU <avustjuzhanin@yandex.ru>
2023-03-22 15:32:39 +0300

    New test: написать свои тесты для UserService

---
+++ src/test/java/ru/yandex/practicum/filmorate/service/UserServiceTest.java
@@ -0,0 +1,69 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+    User firstUser;
+    UserService userService;
+
+    @BeforeEach
+    public void beforeEach() {
+        firstUser = new User(0, "qw@qw.ru", "Login", "", "1900-03-25");
+        userService = new UserService(new InMemoryUserStorage());
+    }
+
+    @AfterEach
+    public void afterEach() {
+        userService.getListOfUsers().clear();
+    }
+
+    @Test
+    void shouldGetUserById() {
+        assertThrows(NullPointerException.class, () -> userService.getUserById(1));
+        userService.save(firstUser);
+        firstUser.setId(1);
+        assertEquals(firstUser, userService.getUserById(1), "пользователь не вернулся из хранилища по id ");
+    }
+
+    @Test
+    void shouldAddFriendsAndGetThem() {
+        assertThrows(NullPointerException.class, () -> userService.getFriends(2));
+        assertThrows(NullPointerException.class, () -> userService.getCommonFriends(1, 2));
+
+        User friendUser = new User(0, "friend@friend.ru", "Login", "Friend", "1963-03-25");
+        userService.save(firstUser);
+        userService.save(friendUser);
+        userService.addFriends(firstUser.getId(), friendUser.getId());
+
+        Set<Integer> friendOfFirst = new HashSet<>();
+        friendOfFirst.add(friendUser.getId());
+        Set<Integer> friendOfFriend = new HashSet<>();
+        friendOfFriend.add(firstUser.getId());
+
+        assertEquals(friendOfFirst, userService.getFriends(firstUser.getId()), "новый друг не добавлен");
+        assertEquals(friendOfFriend, userService.getFriends(friendUser.getId()), "нарушен принцип взаимности " +
+                "при добавлении в друзья");
+    }
+
+    @Test
+    void shouldGetCommonFriends() {
+        User friendUser = new User(0, "friend@friend.ru", "Login", "Friend", "1963-03-25");
+        User thirdUser = new User(0, "third@user.ru", "Third", "ThirdU", "1983-01-15");
+        userService.save(firstUser);
+        userService.save(friendUser);
+        userService.save(thirdUser);
+        userService.addFriends(firstUser.getId(), friendUser.getId());
+        userService.addFriends(thirdUser.getId(), friendUser.getId());
+        assertEquals(userService.getCommonFriends(1,3), userService.getCommonFriends(3,1));
+    }
+}
