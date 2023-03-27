f82fc6203693dfca8560f44c08c09579db641a2b
Andrey-VU <avustjuzhanin@yandex.ru>
2023-03-27 13:05:16 +0300

    CodeStyle: reformat code

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
+
 import javax.validation.Valid;
 import java.util.Collection;
 import java.util.Map;
@@ -34,7 +35,7 @@ public class FilmController {
     }
 
     @PutMapping("/{id}/like/{userId}")
-    public Film likeFilm(@PathVariable int id, @PathVariable int userId){
+    public Film likeFilm(@PathVariable int id, @PathVariable int userId) {
         return filmService.addLike(filmService.getFilmById(id), userId);
     }
 
@@ -43,19 +44,19 @@ public class FilmController {
         return filmService.getFilms();
     }
 
-    @GetMapping(value = { "/popular", "/popular/{count}" })
+    @GetMapping(value = {"/popular", "/popular/{count}"})
     public Collection<Film> getTopFilmsWithOptional(@RequestParam Optional<String> count) {
         return count.isPresent() ? filmService.getTopFilms(Integer.parseInt(count.get())) :
                 filmService.getTopFilms(10);
     }
 
     @GetMapping("/{id}")
-    public Film getFilmById(@PathVariable int id){
+    public Film getFilmById(@PathVariable int id) {
         return filmService.getFilmById(id);
     }
 
     @DeleteMapping("/{id}/like/{userId}")
-    public Film unlikeFilm(@PathVariable int id, @PathVariable int userId){
+    public Film unlikeFilm(@PathVariable int id, @PathVariable int userId) {
         return filmService.deleteLike(getFilmById(id), userId);
     }
 
