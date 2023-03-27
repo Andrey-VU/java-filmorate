6d647ca3a069f2d1b6fad7bc8328e286cc9cd9dd
Andrey-VU <avustjuzhanin@yandex.ru>
2023-03-27 13:08:14 +0300

    CodeStyle: reformat code

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
